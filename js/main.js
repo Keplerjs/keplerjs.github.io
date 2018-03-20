7d080ea81bd7d1a092070c453a1c8dc1cf76bd81
stefanocudini <stefano.cudini@gmail.com>
2018-03-21 00:24:08 +0100

    stats

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ var map = L.map('map', {
 
 var stats = L.featureGroup().addTo(map);
 
-var host = 'http://demo.keplerjs.local';
+var host = 'https://demo.keplerjs.io';
 
 $.when(
 	$.getJSON('https://unpkg.com/geojson-resources@1.1.0/world.json'),
